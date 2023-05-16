37890872d6170ac51849ee2f6e1c076a1f8ff980
SymAeN123 <ameya.naik.storm@gmail.com>
2023-05-15 18:03:09 -0700

    changed tab to 4 spaces + did icon

--- frontend/111forecast/vite.config.js
+++ frontend/111forecast/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        outDir: '../static',
+    },
+    proxy: {
+    '/query': {
+            target: 'https://ec2-34-230-20-67.compute-1.amazonaws.com/',
+            changeOrigin: true,
+            secure: false,
+            ws: true
+        }
+    }
 })
