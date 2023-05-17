cb4f3773eb5a1f09e50a71074a06625f5f093b5c
SymAeN123 <ameya.naik.storm@gmail.com>
2023-05-17 16:24:47 -0700

    more streamlining to start and kill scripts

--- kill.sh
+++ kill.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Attempting to kill servers if they exist:"
 screen -X -S 111forecast_public_backend quit
 screen -X -S 111forecast_discord quit
 screen -X -S 111forecast_database_api quit
--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ ROOT_DIR=$(pwd)
 
 ./kill.sh
 
+echo "Starting servers..."
 cd "${ROOT_DIR}/backend/database_api/"
 screen -S 111forecast_database_api -d -m bash -c 'conda run --live-stream -n 111forecast python app.py'
 cd $ROOT_DIR
@@ -15,3 +16,4 @@ cd $ROOT_DIR
 cd "${ROOT_DIR}/backend/public_backend/"
 screen -S 111forecast_public_backend -d -m bash -c 'conda run --live-stream -n 111forecast python app.py'
 cd $ROOT_DIR
+echo -e "Servers started. Use \"screen -ls\" to view and \"screen -r {sessionname}\" to enter server sessions."
